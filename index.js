b2bc1166392491dfce77565e4ad20d3ae57ac5c7
Granit <gzymeri55@gmail.com>
2022-01-17 12:23:49 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+let names = ["Guadalupe", "Ollie", "Aki"]
+let events = "surprise"
+    
+function writeCards(names, events){
+    let output = []
+    for (let i = 0; i < names.length; i++){
+        output.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return output
+}
+
+function countDown(){
+    let count = 11
+    while (count > 0){
+        count--;
+       console.log(count); 
+       
+    } 
+}
